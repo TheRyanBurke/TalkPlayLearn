2b1d68f2c0a6975a6dacb2dc62767a9ff1cf418e
Ryan Burke <theryanburke@gmail.com>
2012-02-25 06:02:00 -0700

    added icanhaz for js templating, will probably rewrite all those play tags i made

---
+++ app/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Constants {
+
+	
+	public static final int XPCAPINCREASE = 50;
+	public static final int INITIALXPCAP = 100;
+	
+	public static final String CURRENT_USER = "currentUser";
+}
